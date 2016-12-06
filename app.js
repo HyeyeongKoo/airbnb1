b88bf8f5ed678b4f965073dc6e5c0cccc3e72c9b
Hye Young Koo <Hye Young Koo>
2016-12-06 10:44:10 +0900

    commit for test

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session =require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -20,7 +21,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  resave:true,
+  saveUnitialized: true,
+  secret: 'secret long password for session 2016-12'
+}))
 
 app.use('/', index);
 app.use('/users', users);
